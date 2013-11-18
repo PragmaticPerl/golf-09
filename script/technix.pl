688925008bb0492738a9a48fb2a08e541d7f0fce
Sergei Mozhaisky <sergei.mozhaisky@gmail.com>
2013-11-18 09:40:28 +0000

    perlgolf-09 entry v2.0

--- script/technix.pl
+++ script/technix.pl
@@ -1,2 +1,2 @@
 #!perl -p0
-sub r{my@e;$e["@-" % 11].=$& while/[^E]/gs;@e}sub x{s/(?<=[^X]{6}.{5}[^X])[ @]{4,}(?=[^X].{5}[^X]{6})/'@'x length$&/egs}$_="E"x11 .$_."E"x20;s/\n/E/g;x;$_="E"x11 .join("E",r)."E"x20;x;$_=join"E",r;s/E/\n/g
+sub r{$_=$z.$_.$z;s/(?<=[^X]{6}.{5}[^X]) (?=   [^X].{5}[^X]{6})/@/gs;my@e;$e["@-"%11].=$&while/[^\n]/g;$_=join"\n",@e}$z="\n"x22;r;r
