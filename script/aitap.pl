c08665a87d2b14393b37eaaf6afe9ed0e01747cb
Ivan Krylov <krylov.r00t@gmail.com>
2013-11-30 23:36:58 +0400

    compress

--- script/aitap.pl
+++ script/aitap.pl
@@ -1,4 +1,2 @@
 #!perl -p0
-sub z:lvalue{substr$_,$a+($;=pos()?pos()-1:$;),1}while(/X/g){for
-$a(-12,-11,-10,-1,1,10,11,12){z&&z=~s/ /*/}}s/(   ) /$1@/g;
-1 while s/(( .{10}){3}) /$1@/sgc;
+sub z:lvalue{substr$_,$a+pos()-1,1}while(/X/g){for$a(-12..-10,-1,1,10..12){z&&z=~s/ /*/}}s/(   ) /$1@/g;{s/(( .{10}){3}) /$1@/sgc&&redo}
